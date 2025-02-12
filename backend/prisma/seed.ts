f1b231446733ef3abd40b8cd80d3a6679f3a17e7
Alvaro <almartin@fi.uba.ar>
2025-02-12 20:18:46 -0300

    correciones varias

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -19,17 +19,13 @@ interface Property {
 const prisma = new PrismaClient();
 
 const run = async () => {
-  console.log('Seeding data');
   try {
     const responseLogs = fs.readFileSync('prisma/mock_logs.json', 'utf8');
     const logs: LogEntry[] = JSON.parse(responseLogs);
 
     const responseProperties = fs.readFileSync('prisma/mock_properties.json', 'utf8');
     const properties: Property[] = JSON.parse(responseProperties);
-    
-    console.log(logs);
-    console.log(properties);
-
+  
     for (const property of properties) {
       await prisma.generalProperty.create({
         data: {
@@ -40,20 +36,16 @@ const run = async () => {
     }
 
     for (const log of logs) {
-      try {
-        await prisma.monitoringLog.create({
-          data: {
-            id: log.id,
-            service: log.service,
-            task: log.task,
-            property_id: log.property_id,
-            datetime: new Date(log.datetime),
-            status: log.status
-          }
-        });
-      } catch(e) {
-        console.log(log)
-      }
+      await prisma.monitoringLog.create({
+        data: {
+          id: log.id,
+          service: log.service,
+          task: log.task,
+          property_id: log.property_id,
+          datetime: new Date(log.datetime),
+          status: log.status
+        }
+      });
     }
   } catch (error) {
     console.error(error);
@@ -63,4 +55,4 @@ const run = async () => {
   console.log('Seed completed');
 }
 
-run().catch(e => console.error)
+run();
